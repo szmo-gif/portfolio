bc5219c65bc4cd907ef19754fec00319893f230a
Szmoniewski Baptiste <151198403+szmo-gif@users.noreply.github.com>
2024-11-03 20:21:44 +0100

    📦 change base

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'https://szmo-gif.github.io/portfolio/',
+  base: 'portfolio/',
   plugins: [react()],
 });
