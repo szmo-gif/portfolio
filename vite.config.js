e2cb3c2a2d2e1184da98f4e3ad20b6c25f9d7b56
Szmoniewski Baptiste <151198403+szmo-gif@users.noreply.github.com>
2024-11-03 20:07:50 +0100

    📦 correction gh pages for vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: 'https://szmo-gif.github.io/portfolio/',
   plugins: [react()],
-})
+});
