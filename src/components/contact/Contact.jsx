a9962d73e8492d8805b58cc1dcdb1db14ceec1c8
Szmoniewski Baptiste <151198403+szmo-gif@users.noreply.github.com>
2024-11-04 10:48:37 +0100

    🚧 Contact > change url facebook

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -7,7 +7,7 @@ import IconNav from '../../ui/IconNav';
 
 const Contact = () => {
   const navLinks = [
-    { href: "https://facebook.com/baptiste.szmoniewski", icone: "fab fa-facebook", style: "social-icon facebook"},
+    { href: "https://www.facebook.com/baptiste.szmoniewski.9/", icone: "fab fa-facebook", style: "social-icon facebook"},
     { href: "https://instagram.com/baptiste.szmoniewski", icone: "fab fa-instagram", style: "social-icon instagram"},
   ]
   return (
